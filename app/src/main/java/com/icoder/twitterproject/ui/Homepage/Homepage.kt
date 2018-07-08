bd3c31870aa1db0d42ae0332f690a4d5ef6938ec
Dosya <alybaev_d@auca.kg>
2018-07-08 21:38:07 +0600

    cleaning code,before to start add fucntion refresh

--- app/src/main/java/com/icoder/twitterproject/ui/Homepage/Homepage.kt
+++ app/src/main/java/com/icoder/twitterproject/ui/Homepage/Homepage.kt
@@ -3,35 +3,33 @@ package com.icoder.twitterproject.ui.Homepage
 import android.os.Bundle
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.View
 import com.bumptech.glide.Glide
 import com.icoder.twitterproject.R
-import com.icoder.twitterproject.R.id.image_profile
 import com.icoder.twitterproject.R.layout.homepage
 import com.icoder.twitterproject.utils.Constants.Companion.KEY_USER_ID
 import com.icoder.twitterproject.utils.Constants.Companion.KEY_USER_NAME
 import com.joooonho.SelectableRoundedImageView
-import com.squareup.picasso.Picasso
-import com.twitter.sdk.android.core.Callback
-import com.twitter.sdk.android.core.Result
-import com.twitter.sdk.android.core.TwitterCore
-import com.twitter.sdk.android.core.TwitterException
+import com.twitter.sdk.android.core.*
 import com.twitter.sdk.android.core.models.Tweet
+import com.twitter.sdk.android.core.services.StatusesService
 import com.twitter.sdk.android.tweetcomposer.TweetComposer
 import com.twitter.sdk.android.tweetui.TweetUtils
 import com.twitter.sdk.android.tweetui.TweetView
 import kotlinx.android.synthetic.main.homepage.*
 import kotlinx.android.synthetic.main.toolbar.*
-import java.util.*
-import kotlin.collections.ArrayList
 
 
 class Homepage : AppCompatActivity() {
 
     var userId: Long? = null
     var userName: String? = null
+
+    private var twitterApiClient: TwitterApiClient? = null
+    private var tweetHomepage: List<Tweet>? = null
+    private var statusesService: StatusesService? = null
+
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(homepage)
@@ -48,11 +46,16 @@ class Homepage : AppCompatActivity() {
     fun init() {
         initToolbar()
         initReceivedData()
-        callTwitterApiClient()
+        initTweets()
         initProfileImage()
         swipeLayoutListener()
     }
 
+    private fun initTweets() {
+        initTwitterHomelineServices()
+        callTwitterApiClient()
+        addTweetIntoLayout()
+    }
 
 
     private fun initProfileImage() {
@@ -67,27 +70,25 @@ class Homepage : AppCompatActivity() {
         userName = intent.getStringExtra(KEY_USER_NAME)
         title = "@" + userName
 
-
         userId = intent.getLongExtra(KEY_USER_ID, 0)
     }
 
+    fun initTwitterHomelineServices(){
+        twitterApiClient = TwitterCore.getInstance().apiClient
+        statusesService = twitterApiClient!!.statusesService
+    }
+
+
+
+
     fun callTwitterApiClient() {
-        val twitterApiClient = TwitterCore.getInstance().apiClient
-        val statusesService = twitterApiClient.statusesService
-        val call = statusesService.homeTimeline(null, null, null, true, true, false, true)
+
+        val call = statusesService!!.homeTimeline(null, null, null, true, true, false, true)
         call.enqueue(object : Callback<List<Tweet>>() {
             override fun success(result: Result<List<Tweet>>) {
-                for(i in 0 until result.data.size){
-                    val tweetId = result.data[i].id
-                    TweetUtils.loadTweet(tweetId, object:Callback<Tweet>() {
-                        override fun success(result:Result<Tweet>) {
-                            tweet_layout.addView(TweetView(this@Homepage, result.data))
-                            swipe_layout_refresh.isRefreshing = false
-                        }
-                        override fun failure(exception:TwitterException) {
-                            swipe_layout_refresh.isRefreshing = false
-                        }
-                    })
+                for (i in 0 until result.data.size) {
+                    tweetHomepage = result.data
+
                 }
 
             }
@@ -97,44 +98,34 @@ class Homepage : AppCompatActivity() {
             }
         })
     }
-    fun swipeLayoutListener(){
+    fun addTweetIntoLayout(){
+        for(i in 0 until tweetHomepage!!.size) {
+            TweetUtils.loadTweet(tweetHomepage!![i].id, object : Callback<Tweet>() {
+                override fun success(result: Result<Tweet>) {
+                    tweet_layout.addView(TweetView(this@Homepage, result.data))
+
+                }
+
+                override fun failure(exception: TwitterException) {
+
+                }
+            })
+        }
+    }
+    fun swipeLayoutListener() {
         swipe_layout_refresh.setOnRefreshListener(object : SwipeRefreshLayout.OnRefreshListener {
             override fun onRefresh() {
                 swipe_layout_refresh.isRefreshing = true
-                callTwitterApiClient()
+                refreshTweets()
             }
         })
     }
 
-//    fun initTwitterListAdapter(data: List<Tweet>) {
-//        var layoutManager = LinearLayoutManager(this)
-//        list_tweets.layoutManager = layoutManager
-//        val dividerItemDecoration = DividerItemDecoration(list_tweets.context, layoutManager.orientation)
-//        list_tweets.addItemDecoration(dividerItemDecoration)
-//        val userTimeline = FixedTweetTimeline.Builder()
-//                .setTweets(data)
-//                .build()
-//
-//        val adapter = TweetTimelineRecyclerViewAdapter.Builder(this)
-//                .setTimeline(userTimeline)
-//                .setViewStyle(R.style.tw__TweetLightStyle)
-//                .build()
-//        list_tweets.adapter = adapter
-//        swipe_layout_refresh.setOnRefreshListener(object : SwipeRefreshLayout.OnRefreshListener {
-//            override fun onRefresh() {
-//                swipe_layout_refresh.isRefreshing = true
-//                adapter!!.refresh(object : Callback<TimelineResult<Tweet>>() {
-//                    override fun success(result: Result<TimelineResult<Tweet>>) {
-//                        swipe_layout_refresh.isRefreshing = false
-//                    }
-//
-//                    override fun failure(exception: TwitterException) {
-//                        "There are no new tweets"
-//                    }
-//                })
-//            }
-//        })
-//    }
+    private fun refreshTweets() {
+        callTwitterApiClient()
+
+    }
+
 
     private fun initToolbar() {
         setSupportActionBar(toolbar)
